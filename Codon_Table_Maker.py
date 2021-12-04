c002b0e27cda2abe2429a9807ce5c7a7591fe32a
Arda-Berk <95045907+Arda-Berk@users.noreply.github.com>
2021-12-04 16:25:01 +0300

    Update Codon_Table_Maker.py

--- Codon_Table_Maker.py
+++ Codon_Table_Maker.py
@@ -1,8 +1,6 @@
-R1 = ["U","C","A","G"]
-R2 = ["U","C","A","G"]
-R3 = ["U","C","A","G"]
+R = ["U","C","A","G"]
 
-for a in R1:
-  for b in R2:
-    for c in R3:
+for a in R:
+  for b in R:
+    for c in R:
       print("'{}':'',".format(a + b + c))
